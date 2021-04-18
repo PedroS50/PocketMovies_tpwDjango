2ca3b5afb91b4e8016ef57e36f9ad0041b9be846
Pedro <pedro.miguel50@ua.pt>
2021-04-18 19:46:38 +0100

    Changed user to profile.

--- PocketMovies/app/models.py
+++ PocketMovies/app/models.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import User
 class Genre(models.Model):
 	name = models.CharField(max_length=32)
 
-class User(models.Model):
+class Profile(models.Model):
 	user_details = models.OneToOneField(User, on_delete=models.CASCADE)
 	profile_picture = models.ImageField(blank=True, null=True)
 	favorite_genres = models.ManyToManyField(Genre, blank=True, null=True)
 	favorite_movies = models.ManyToManyField(Movie, blank=True, null=True)
 	movies_watched = models.ManyToManyField(Movie, blank=True, null=True)
 	want_to_watch = models.ManyToManyField(Movie, blank=True, null=True)
-	
