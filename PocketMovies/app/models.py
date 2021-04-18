0316c6eb30b5d469c24c7bc93443d28104b05a5c
oscar fernandez <oscar7fernandez@outlook.com>
2021-04-18 12:14:16 +0100

    Producer and Actor models and Views

--- PocketMovies/app/models.py
+++ PocketMovies/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    birthdate = models.DateField()
+    years_active = models.CharField(max_length=2)
+    nationality = models.CharField(max_length=50)
+    ##twitter account?
+    def __str__(self):
+        return self.name
+
+class Producer(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+    def __str__(self):
+        return self.name
--- PocketMovies/app/views.py
+++ PocketMovies/app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from app.models import *
+
 
 # Create your views here.
+def home(request):
+    return render(request, "layout.html")
+
+
+def infoProducer(request, id):
+    try:
+        producer = Producer.objects.get(id=id)
+        return render(request, "infoProducer.html", {"producer": producer})
+    except:
+        producer = None
+        return render(request, "infoProducer.html")
+
+def infoActor(request,id):
+    try:
+        actor = Actor.objects.get(id=id)
+        return render(request,"infoActor.html",{"actor":actor})
+    except:
+        producer = None
+        return render(request,"infoActor.html")
