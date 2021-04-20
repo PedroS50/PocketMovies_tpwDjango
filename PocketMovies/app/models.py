16c090824a6ddf5459e13cd20d3c42fcc7f439a1
Pedro <pedro.miguel50@ua.pt>
2021-04-20 19:17:01 +0100

    Changes to movies and actors pages

--- PocketMovies/app/models.py
+++ PocketMovies/app/models.py
@@ -17,18 +17,4 @@ class Profile(models.Model):
     favorite_movies = models.ManyToManyField(Movie, related_name='user_favorite_movies')
     movies_watched = models.ManyToManyField(Movie, related_name='user_watched_movies')
     want_to_watch = models.ManyToManyField(Movie, related_name='user_wanttowatch_movies')
-# favorite_movies = models.ManyToManyField(Movie, through='FavoriteMovies')
-# movies_watched = models.ManyToManyField(Movie, through='MoviesWatched')
-# want_to_watch = models.ManyToManyField(Movie, through='WantToWatch')
 
-# class FavoriteMovies(models.Model):
-# 	user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-# 	movie = models.ForeignKey(Profile, on_delete=models.CASCADE)
-
-# class MoviesWatched(models.Model):
-# 	user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-# 	movie = models.ForeignKey(Profile, on_delete=models.CASCADE)
-
-# class WantToWatch(models.Model):
-# 	user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-# 	movie = models.ForeignKey(Profile, on_delete=models.CASCADE)
